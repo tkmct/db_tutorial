caa0c0505d99f6cb8f5702666be80e7712d162ca
tkmct <1220t.takamichi@gmail.com>
2020-08-30 16:04:21 +0900

    skelton

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,58 @@
 use std::io::{self, prelude::*};
 
+enum MetaCommandResult {
+    Success,
+    Exited,
+    Fail(String),
+}
+
+fn do_meta_command(command: &str) -> MetaCommandResult {
+    use MetaCommandResult::*;
+
+    match command {
+        ".exit" => Exited,
+        _ => Fail(format!("unrecognizable command `{}`", command)),
+    }
+}
+
+enum StatementKind {
+    Insert,
+    Select,
+}
+
+struct Statement {
+    kind: StatementKind,
+}
+
+type StatementError = String;
+
+impl Statement {
+    pub fn prepare(input: &str) -> Result<Self, StatementError> {
+        // TODO: refactor to use function to match first word in input
+        if input.starts_with("select") {
+            Ok(Statement {
+                kind: StatementKind::Select,
+            })
+        } else if input.starts_with("insert") {
+            Ok(Statement {
+                kind: StatementKind::Insert,
+            })
+        } else {
+            Err(String::from("invalid input"))
+        }
+    }
+
+    pub fn execute(&self) {
+        match self.kind {
+            StatementKind::Insert => println!("This is where we do an insert."),
+            StatementKind::Select => println!("This is where we do an select."),
+        }
+    }
+}
+
 fn main() -> Result<(), io::Error> {
+    use MetaCommandResult::*;
+
     loop {
         print!("db > ");
         io::stdout().flush()?;
@@ -9,12 +61,26 @@ fn main() -> Result<(), io::Error> {
         let mut buffer = String::new();
 
         stdin.read_line(&mut buffer).expect("Could not read line");
+        let buffer = buffer.trim();
 
-        match buffer.trim() {
-            ".exit" => break,
-            _ => {
-                println!("Unrecognized command `{}`.", buffer.trim());
-                continue;
+        if buffer.starts_with('.') {
+            match do_meta_command(buffer) {
+                Success => continue,
+                Exited => break,
+                Fail(reason) => {
+                    println!("Fail: {}", reason);
+                    continue;
+                }
+            }
+        }
+
+        match Statement::prepare(buffer) {
+            Ok(statement) => {
+                statement.execute();
+                println!("Executed.");
+            }
+            Err(e) => {
+                println!("Error preparing statement. {}", e);
             }
         }
     }
